aaada8039e568dfb267ed28e08bc5f13930817b8
Edis <edis.vilja@smail.th-koeln.de>
2026-03-01 01:28:15 +0100

    add tailwind

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50">
+      <h1 className="text-4xl font-bold text-gray-900 mb-6">
+        Hello World mit Tailwind!
+      </h1>
+
+      <button
+        className="px-6 py-3 bg-gray-500 text-white font-semibold rounded-lg shadow hover:bg-blue-600 transition"
+        onClick={() => setCount(count + 1)}
+      >
+        Du hast {count} mal geklickt
+      </button>
+
+      <p className="mt-4 text-gray-700">
+        Edit <code className="bg-gray-200 px-1 rounded">src/App.tsx</code> und
+        speichere, um HMR zu testen
       </p>
-    </>
+    </div>
   )
 }
 
